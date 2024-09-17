9a58942582ab4d7c61a3199bf92e4025e0b1c278
iskender <iskender.colak.jd@gmail.com>
2024-09-17 11:10:50 +0300

    DevOps Pipeline

---
+++ src/main/java/com/icolak/devops002pipeline/AppMain.java
@@ -0,0 +1,13 @@
+package com.icolak.devops002pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class, args);
+    }
+
+}
---
+++ src/test/java/com/icolak/devops002pipeline/Devops002PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icolak.devops002pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops002PipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
